5d87591954b375c959bbe2973f0b6437a5be79f4
William Ferreira <109122408+Willchef@users.noreply.github.com>
2022-10-11 21:39:43 -0300

    Correção variáveis de filtro
    Não estava selecionando a opção todos, correção rapida.

--- script.js
+++ script.js
@@ -2,38 +2,38 @@
 
 
 
-// element toggle function
+// função de alternância de elemento
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 
 
-// sidebar variables
+// sidebar variáveis
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
+// sidebar alternar a funcionalidade para celular
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
 
-// testimonials variables
+// testimonials variáveis
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
-// modal variable
+// modal variáveis
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
-// modal toggle function
+// modal função de alternância
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
-// add click event to all modal items
+// adicionar evento de clique a todos os itens modais
 for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
@@ -49,13 +49,13 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// add click event to modal close button
+// adicionar evento de clique ao botão de fechamento modal
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
-// custom select variables
+// variáveis de seleção personalizadas
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
@@ -63,7 +63,7 @@ const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
 
-// add event in all select items
+// adicionar evento em todos os itens selecionados
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
@@ -75,14 +75,14 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
-// filter variables
+// variáveis de filtro
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
 
-    if (selectedValue === "all") {
+    if (selectedValue === "todos") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
       filterItems[i].classList.add("active");
@@ -94,7 +94,7 @@ const filterFunc = function (selectedValue) {
 
 }
 
-// add event in all filter button items for large screen
+// adicionar evento em todos os itens do botão de filtro para tela grande
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -115,16 +115,16 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 
 
-// contact form variables
+// variáveis do formulário de contato
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
+// adicionar evento a todos os campos de entrada do formulário
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
-    // check form validation
+    // verificar a validação do formulário
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
@@ -136,11 +136,11 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 
-// page navigation variables
+// variáveis de navegação de página
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
+// adicionar evento a todos os links de navegação
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
